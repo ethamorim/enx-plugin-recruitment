e3e53ffe229bfd0c352d82e9d683b588a2d29ec4
ethamorim <ethamorim@gmail.com>
2024-07-27 13:32:46 -0300

    feat: connect to database on plugin enable

--- mc-home/src/main/java/com/ethamorim/home/HomePlugin.java
+++ mc-home/src/main/java/com/ethamorim/home/HomePlugin.java
@@ -9,15 +9,9 @@ public class HomePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("Hellooooo from Homie Plugin!");
         var configuration = new Configuration()
-                .setProperty(JAKARTA_JDBC_URL, "jdbc:mariadb://localhost:3306/home")
-                .setProperty(JAKARTA_JDBC_USER, "root")
-                .setProperty(JAKARTA_JDBC_PASSWORD, "")
+                .setProperty(JAKARTA_JDBC_DRIVER, "org.mariadb.jdbc.Driver")
                 .setProperty(JAKARTA_HBM2DDL_DATABASE_ACTION, Action.CREATE)
-                .setProperty(SHOW_SQL, true)
-                .setProperty(FORMAT_SQL, true)
-                .setProperty(HIGHLIGHT_SQL, true)
                 .setProperty("hibernate.agroal.maxSize", 5);
         try (var sessionFactory = configuration.buildSessionFactory()) {
             sessionFactory.getSchemaManager().exportMappedObjects(true);
